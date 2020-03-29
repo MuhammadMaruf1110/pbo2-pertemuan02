dc97193c6c89942ea236c3925473bf69697d6ca5
MuhammadMaruf1110 <62388555+MuhammadMaruf1110@users.noreply.github.com>
2020-03-29 17:11:42 +0700

    Add files via upload

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilaninfo(){
+   System.out.println();
+   System.out.println("==Data Mahasiswa ===============");
+   System.out.println("NPM : 18630361");
+   System.out.println("Nama : maruf");
+   System.out.println("==========================");
+ }
+}   
